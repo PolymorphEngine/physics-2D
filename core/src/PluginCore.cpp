c31a2ac05bafb0a2c1ef406b2377e014dfb4c02e
Tristan Gremont <tristan.gremont@epitech.eu>
2023-01-17 16:39:25 +0100

    fix(core): missing collision handling logic

--- core/src/PluginCore.cpp
+++ core/src/PluginCore.cpp
@@ -2,6 +2,7 @@
 #include "ComponentFactory.hpp"
 #include "SerializableObjectFactory.hpp"
 #include "polymorph/engine/api/plugin/Symbols.hpp"
+#include "ACollider2dComponent.hpp"
 
 namespace polymorph::engine::api
 {
@@ -17,10 +18,38 @@ namespace polymorph::engine::api
 
     void PluginCore::updateInternalSystems(std::shared_ptr<Scene> &scene)
     {
+        auto colliders = scene->getComponentsInScene<physics2D::ACollider2dComponent>();
+
+        for (auto iterator = colliders.begin();
+             iterator != colliders.end(); ++ iterator)
+        {
+            for (auto normal_iterator = iterator + 1;
+                 normal_iterator != colliders.end(); ++normal_iterator)
+            {
+                (*iterator)->setColliderPosition();
+                (*normal_iterator)->setColliderPosition();
+                if ((*iterator)->enabled)
+                    physics2D::ACollider2dComponent::checkCollision(*iterator, *normal_iterator);
+                if (_game.isExiting() || _game.getSceneManager().isSceneUnloaded())
+                    return;
+                if ((*normal_iterator)->enabled)
+                    physics2D::ACollider2dComponent::checkCollision(*normal_iterator, *iterator);
+                if (_game.isExiting() || _game.getSceneManager().isSceneUnloaded())
+                    return;
+            }
+        }
     }
 
     void PluginCore::postUpdateInternalSystems(std::shared_ptr<Scene> &scene)
     {
+        if (_game.isDebugMode())
+            return;
+        auto colliders = scene->getComponentsInScene<physics2D::ACollider2dComponent>();
+        for (auto &collider: colliders)
+        {
+            if ((*collider)->enabled)
+                (*collider)->debugDraw();
+        }
     }
 
     void PluginCore::startScripts(std::shared_ptr<Scene> &scene)
